7b94b37d6f70f91711456ddf5925d409ac355079
zaqwsx55 <zaqwsx@poczta.fm>
2017-10-08 11:52:20 +0200

    Aplikacja skończona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+
+import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
+
+import { ModelsService } from './models.service';
+import { Model } from './models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  models: Model[];
+  selectedModel: Model;
+  isDarkTheme = false;
+
+  constructor(private modelsService: ModelsService,
+              private vcr: ViewContainerRef,
+              private mdDialog: MdDialog) {}
+
+  ngOnInit() {
+    this.models = this.modelsService.getAll();
+  }
+
+  showDetails(model: Model) {
+    this.selectedModel = model;
+  }
+
+  addMessage() {
+    // Open dialog to add message
+    // 1. create dialog config
+    const dialogConfig = new MdDialogConfig();
+    dialogConfig.viewContainerRef = this.vcr;
+    // 2. open dialog
+    const dialog = this.mdDialog.open(AddMessageComponent, dialogConfig);
+    // 3. pass selected model to dialog
+    (<any>dialog.componentInstance).selectedModel = this.selectedModel;
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+  }
+
+}
+
+@Component({
+  selector: 'app-add-message',
+  template: `
+  <form (submit)="addMessage()">
+    <md-input-container>
+      <input mdInput name="message" [(ngModel)]="message" placeholder="Message">
+    </md-input-container>
+    <button md-raised-button color="accent">Add Message</button>
+  </form>
+  `
+})
+export class AddMessageComponent {
+
+  message = '';
+
+  constructor(private mdDialogRef: MdDialogRef<AddMessageComponent>) {}
+
+  addMessage() {
+    // 1. create message
+    const newMessage = { who: 'John Doe', content: this.message };
+    // 2. add message to selected model
+    const selectedModel: Model = (<any>this.mdDialogRef.componentInstance).selectedModel;
+    selectedModel.messages.push(newMessage);
+    // 3. close dialog
+    this.mdDialogRef.close();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule } from '@angular/material';
+import { MdSidenavModule } from '@angular/material';
+import { MdTabsModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdGridListModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdCardModule } from '@angular/material';
+import { MdDialogModule} from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdMenuModule } from '@angular/material';
+
+import { AppComponent, AddMessageComponent } from './app.component';
+import { ModelsService } from './models.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMessageComponent
+  ],
+  entryComponents: [
+    AppComponent,
+    AddMessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdTabsModule,
+    MdIconModule,
+    MdGridListModule,
+    MdButtonModule,
+    MdCardModule,
+    MdDialogModule,
+    MdInputModule,
+    MdMenuModule
   ],
-  providers: [],
+  providers: [ModelsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
